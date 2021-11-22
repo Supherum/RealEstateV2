b5d7748380cd5a684bc39b8fc8115e8cc2476493
Supherum <supherum@gmail.com>
2021-11-22 22:27:03 +0100

    minor bug fixed

--- realestate.v2/src/main/java/com/salesianos/triana/realestate/v2/vivienda/service/ViviendaService.java
+++ realestate.v2/src/main/java/com/salesianos/triana/realestate/v2/vivienda/service/ViviendaService.java
@@ -4,12 +4,15 @@ package com.salesianos.triana.realestate.v2.vivienda.service;
 import com.salesianos.triana.realestate.v2.shared.service.BaseService;
 import com.salesianos.triana.realestate.v2.vivienda.model.Vivienda;
 import com.salesianos.triana.realestate.v2.vivienda.repository.ViviendaRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class ViviendaService extends BaseService<Vivienda, UUID, ViviendaRepository> {
 
+    public List<Vivienda> viviendaConSpecification (Specification<Vivienda> spec){return  repository.findAll(spec);}
 
 }
